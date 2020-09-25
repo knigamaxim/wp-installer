17e4eb8f1291b58ee2fbd18d222a088f270ed289
knigamaxim <maxwpdev@gmail.com>
2020-09-25 21:05:26 +0300

    add constructor

--- WPinstaller.php
+++ WPinstaller.php
@@ -8,4 +8,11 @@ class WpInstaller
 	protected $envFile;
 	protected $wpResponse;
 
+	function __construct()
+	{
+		$this->setErrorHandler();
+		$this->envFile = dirname(__DIR__) . '/_env';
+		$this->setEnvParams();
+	}
+
 }
